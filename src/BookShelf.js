3a3e1c8648100fb2493cf0f230dd1714534de3e3
Khaled Sarhan <khaled_sarhan2003@yahoo.com>
2018-07-25 17:24:04 +0300

    Add a non books message to the shelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -7,30 +7,37 @@ class BookShelf extends React.Component {
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
                 <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {this.props.books.map(book => (
-                            <li key={book.id}>
-                                <div className="book">
-                                    <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.thumbnail + '")' }}></div>
-                                        <div className="book-shelf-changer">
-                                            <select value={book.shelf} onChange={(target) => this.props.onBookMove(book, target.currentTarget.value)}>
-                                                <option value="move" disabled>Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read">Read</option>
-                                                <option value="none">None</option>
-                                            </select>
+                    {(this.props.books && this.props.books.length > 0) ? (
+                        <ol className="books-grid">
+                            {this.props.books.map(book => (
+                                <li key={book.id}>
+                                    <div className="book">
+                                        <div className="book-top">
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.thumbnail + '")' }}></div>
+                                            <div className="book-shelf-changer">
+                                                <select value={book.shelf} onChange={(target) => this.props.onBookMove(book, target.currentTarget.value)}>
+                                                    <option value="move" disabled>Move to...</option>
+                                                    <option value="currentlyReading">Currently Reading</option>
+                                                    <option value="wantToRead">Want to Read</option>
+                                                    <option value="read">Read</option>
+                                                    <option value="none">None</option>
+                                                </select>
+                                            </div>
                                         </div>
+                                        <div className="book-title">{book.title}</div>
+                                        {book.authors.map(author => {
+                                            <div className="book-authors">{author}</div>
+                                        })}
                                     </div>
-                                    <div className="book-title">{book.title}</div>
-                                    {book.authors.map(author => {
-                                        <div className="book-authors">{author}</div>
-                                    })}
-                                </div>
-                            </li>
-                        ))}
-                    </ol>
+                                </li>
+                            ))}
+                        </ol>
+                    ) : (
+                            <div className="empty">
+                                <p>There is no books here</p>
+                            </div>
+                        )}
+
                 </div>
             </div>
         )
